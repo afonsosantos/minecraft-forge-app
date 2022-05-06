0debb43dabb5f0a84a2c7a2fe28ee302e8d39378
Girish Ramakrishnan <girish@cloudron.io>
2022-05-06 12:41:02 -0700

    Remove redundant if block

--- start.sh
+++ start.sh
@@ -15,21 +15,19 @@ if [[ ! -f ${SETTINGS_FILE} ]]; then
     cp /app/code/server.properties.template ${SETTINGS_FILE}
 fi
 
-if [[ -f ${SETTINGS_FILE} ]]; then
-    echo "=> Update server port"
-    sed -e "s/server-port.*/server-port=${SERVER_PORT}/" -i ${SETTINGS_FILE}
-    echo "=> Update query port"
-    sed -e "s/query.port.*/query.port=${SERVER_PORT}/" -i ${SETTINGS_FILE}
+echo "=> Update server port"
+sed -e "s/server-port.*/server-port=${SERVER_PORT}/" -i ${SETTINGS_FILE}
+echo "=> Update query port"
+sed -e "s/query.port.*/query.port=${SERVER_PORT}/" -i ${SETTINGS_FILE}
 
-    if [[ -z "${RCON_PORT:-}" ]]; then
-        echo "=> Disable rcon port"
-        sed -e "s/rcon.port.*/rcon.port=/" -i ${SETTINGS_FILE}
-        sed -e "s/enable-rcon.*/enable-rcon=false/" -i ${SETTINGS_FILE}
-    else
-        echo "=> Update rcon port"
-        sed -e "s/rcon.port.*/rcon.port=${RCON_PORT}/" -i ${SETTINGS_FILE}
-        sed -e "s/enable-rcon.*/enable-rcon=true/" -i ${SETTINGS_FILE}
-    fi
+if [[ -z "${RCON_PORT:-}" ]]; then
+    echo "=> Disable rcon port"
+    sed -e "s/rcon.port.*/rcon.port=/" -i ${SETTINGS_FILE}
+    sed -e "s/enable-rcon.*/enable-rcon=false/" -i ${SETTINGS_FILE}
+else
+    echo "=> Update rcon port"
+    sed -e "s/rcon.port.*/rcon.port=${RCON_PORT}/" -i ${SETTINGS_FILE}
+    sed -e "s/enable-rcon.*/enable-rcon=true/" -i ${SETTINGS_FILE}
 fi
 
 echo "=> Ensure permissions"
